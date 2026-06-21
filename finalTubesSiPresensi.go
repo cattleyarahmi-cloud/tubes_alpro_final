fbe91eb10aa46b8f8d0d021ddd06dc66c937d668
cattleyarahmi-cloud <cattleyarahmin@gmail.com>
2026-06-21 13:40:57 +0700

    Revisi fitur edit data dan sorting

--- finalTubesSiPresensi.go
+++ finalTubesSiPresensi.go
@@ -5,10 +5,11 @@ import "fmt"
 const nmax = 100
 
 type mhs struct {
-	nim   int64
-	nama  string
-	kelas string
-	ket   string
+	nim         int64
+	nama        string
+	kelas       string
+	jadwalKelas string
+	ket         string
 }
 
 type tmhs [nmax]mhs
@@ -18,7 +19,7 @@ func main() {
 	var n, tambah int
 	var pilih int
 	var cariNim int64
-	var cariNama, cariKet string
+	var cariNama, cariKet, cariKelas, cariJadwalKuliah string
 
 	for {
 		fmt.Println("\n===== PRESENSI MAHASISWA =====")
@@ -28,6 +29,7 @@ func main() {
 		fmt.Println("4. Urutkan Data")
 		fmt.Println("5. Statistik")
 		fmt.Println("6. Hapus Data")
+		fmt.Println("7. Edit Data")
 		fmt.Println("0. Keluar")
 		fmt.Print("Pilih menu: ")
 		fmt.Scan(&pilih)
@@ -48,6 +50,8 @@ func main() {
 			fmt.Println("1. Cari dengan NIM")
 			fmt.Println("2. Cari dengan Nama")
 			fmt.Println("3. Cari dengan Keterangan")
+			fmt.Println("4. Cari dengan Kelas")
+			fmt.Println("5. Cari dengan Mata Kuliah")
 			fmt.Print("Pilih: ")
 			var pilih2 int
 			fmt.Scan(&pilih2)
@@ -88,14 +92,26 @@ func main() {
 				fmt.Scan(&cariKet)
 				sequentialSearchKet(n, mahasiswa[:], cariKet)
 
+			case 4:
+				fmt.Print("Masukkan Kelas yang dicari: ")
+				fmt.Scan(&cariKelas)
+				sequentialSearchKelas(n, mahasiswa[:], cariKelas)
+
+			case 5:
+				fmt.Print("Masukkan Mata Kuliah yang dicari: ")
+				fmt.Scan(&cariJadwalKuliah)
+				sequentialSearchJadwalKelas(n, mahasiswa[:], cariJadwalKuliah)
+
 			default:
 				fmt.Println("Pilihan tidak tersedia")
 			}
 
 		case 4:
 			fmt.Println("===== URUTKAN BERDASARKAN =====")
-			fmt.Println("1. Ascending (A-Z)")
-			fmt.Println("2. Descending (Z-A)")
+			fmt.Println("1. Nama Ascending (A-Z)")
+			fmt.Println("2. Nama Descending (Z-A)")
+			fmt.Println("3. NIM Ascending (0-9)")
+			fmt.Println("4. NIM Descending (9-0)")
 			fmt.Print("Pilih: ")
 			var pilih3 int
 			fmt.Scan(&pilih3)
@@ -106,6 +122,12 @@ func main() {
 			} else if pilih3 == 2 {
 				selectionSortMhs(n, mahasiswa[:], false)
 				fmt.Println("Data berhasil diurutkan Descending")
+			} else if pilih3 == 3 {
+				insertionsortbynim(n, mahasiswa[:], true)
+				fmt.Println("Data berhasil diurutkan NIM Ascending")
+			} else if pilih3 == 4 {
+				insertionsortbynim(n, mahasiswa[:], false)
+				fmt.Println("Data berhasil diurutkan NIM Descending")
 			} else {
 				fmt.Println("Pilihan tidak tersedia")
 			}
@@ -115,11 +137,33 @@ func main() {
 			statistik(n, mahasiswa[:])
 
 		case 6:
-			fmt.Print("Masukkan NIM yang akan dihapus: ")
-			fmt.Scan(&cariNim)
-			hapus(&n, mahasiswa[:], cariNim)
-			fmt.Print("Data setelah NIM ", cariNim, " dihapus:")
+			fmt.Println("===== HAPUS BERDASARKAN =====")
+			fmt.Println("1. Hapus dengan NIM")
+			fmt.Println("2. Hapus dengan Nama")
+			fmt.Print("Pilih: ")
+			var pilih4 int
+			fmt.Scan(&pilih4)
+
+			switch pilih4 {
+			case 1:
+				fmt.Print("Masukkan NIM yang akan dihapus: ")
+				fmt.Scan(&cariNim)
+				hapusByNim(&n, mahasiswa[:], cariNim)
+
+			case 2:
+				fmt.Print("Masukkan Nama yang akan dihapus: ")
+				fmt.Scan(&cariNama)
+				hapusByNama(&n, mahasiswa[:], cariNama)
+
+			default:
+				fmt.Println("Pilihan tidak tersedia")
+			}
 			tampil(n, mahasiswa[:])
+
+		case 7:
+			fmt.Print("Masukan NIM yang akan diedit keterangannya :")
+			fmt.Scan(&cariNim)
+			editdata(n, mahasiswa[:], cariNim)
 
 		case 0:
 			fmt.Println("Program selesai")
@@ -141,6 +185,8 @@ func inputmhs(mulai int, jumlah int, mahasiswa []mhs) {
 		fmt.Scan(&mahasiswa[i].nim)
 		fmt.Print("Kelas : ")
 		fmt.Scan(&mahasiswa[i].kelas)
+		fmt.Print("Mata Kuliah : ")
+		fmt.Scan(&mahasiswa[i].jadwalKelas)
 		fmt.Print("Keterangan (Hadir/Izin/Sakit/Alpha) : ")
 		fmt.Scan(&mahasiswa[i].ket)
 	}
@@ -152,6 +198,7 @@ func tampil(n int, mahasiswa []mhs) {
 		fmt.Println("Nama       :", mahasiswa[i].nama)
 		fmt.Println("NIM        :", mahasiswa[i].nim)
 		fmt.Println("Kelas      :", mahasiswa[i].kelas)
+		fmt.Println("Mata Kuliah:", mahasiswa[i].jadwalKelas)
 		fmt.Println("Keterangan :", mahasiswa[i].ket)
 		fmt.Println()
 	}
@@ -164,6 +211,38 @@ func sequentialSearchNama(n int, mahasiswa []mhs, nama string) int {
 		}
 	}
 	return -1
+}
+
+func sequentialSearchKelas(n int, mahasiswa []mhs, kel string) {
+	found := false
+	for i := 0; i < n; i++ {
+		if mahasiswa[i].kelas == kel {
+			fmt.Println("Nama  :", mahasiswa[i].nama)
+			fmt.Println("NIM   :", mahasiswa[i].nim)
+			fmt.Println("Kelas :", mahasiswa[i].kelas)
+			fmt.Println()
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("Tidak ada mahasiswa dengan kelas", kel)
+	}
+}
+
+func sequentialSearchJadwalKelas(n int, mahasiswa []mhs, jadwalKelas string) {
+	found := false
+	for i := 0; i < n; i++ {
+		if mahasiswa[i].jadwalKelas == jadwalKelas {
+			fmt.Println("Nama  :", mahasiswa[i].nama)
+			fmt.Println("NIM   :", mahasiswa[i].nim)
+			fmt.Println("Kelas :", mahasiswa[i].kelas)
+			fmt.Println()
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("Tidak ada mahasiswa dengan mata kuliah", jadwalKelas)
+	}
 }
 
 func sequentialSearchKet(n int, mahasiswa []mhs, ket string) {
@@ -237,8 +316,22 @@ func binarySearchMhs(n int, mahasiswa []mhs, nim int64) int {
 	return -1
 }
 
-func hapus(n *int, mahasiswa []mhs, nim int64) {
+func hapusByNim(n *int, mahasiswa []mhs, nim int64) {
+	selectionSortByNIM(*n, mahasiswa)
 	idx := binarySearchMhs(*n, mahasiswa, nim)
+	if idx == -1 {
+		fmt.Println("Data tidak ditemukan")
+		return
+	}
+	for i := idx; i < *n-1; i++ {
+		mahasiswa[i] = mahasiswa[i+1]
+	}
+	*n = *n - 1
+	fmt.Println("Data berhasil dihapus")
+}
+
+func hapusByNama(n *int, mahasiswa []mhs, nama string) {
+	idx := sequentialSearchNama(*n, mahasiswa, nama)
 	if idx == -1 {
 		fmt.Println("Data tidak ditemukan")
 		return
@@ -312,5 +405,49 @@ func statistik(n int, mahasiswa []mhs) {
 		fmt.Println("Jumlah Alpha :", maxAlpha)
 	} else {
 		fmt.Println("Tidak ada mahasiswa yang Alpha")
+	}
+}
+
+func editdata(n int, mahasiswa []mhs, nim int64) {
+	var idx int
+	idx = binarySearchMhs(n, mahasiswa, nim)
+
+	if idx != -1 {
+		fmt.Println("Data ditemukan")
+		fmt.Println("Nama :", mahasiswa[idx].nama)
+		fmt.Println("Keterangan Lama :", mahasiswa[idx].ket)
+
+		fmt.Print("Keterangan Baru (Hadir/Izin/Sakit/Alpha): ")
+		fmt.Scan(&mahasiswa[idx].ket)
+
+		fmt.Println("Keterangan berhasil diubah")
+		fmt.Println("Nama :", mahasiswa[idx].nama)
+		fmt.Println("Keterangan Baru :", mahasiswa[idx].ket)
+	} else {
+		fmt.Println("Data tidak ditemukan")
+	}
+}
+
+func insertionsortbynim(n int, mahasiswa []mhs, ascend bool) {
+	var pass, i int
+	var temp mhs
+
+	for pass = 1; pass < n; pass++ {
+		temp = mahasiswa[pass]
+		i = pass - 1
+
+		if ascend {
+			for i >= 0 && mahasiswa[i].nim > temp.nim {
+				mahasiswa[i+1] = mahasiswa[i]
+				i--
+			}
+		} else {
+			for i >= 0 && mahasiswa[i].nim < temp.nim {
+				mahasiswa[i+1] = mahasiswa[i]
+				i--
+			}
+		}
+		mahasiswa[i+1] = temp
+
 	}
 }
